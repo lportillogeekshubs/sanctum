a9169753f16765fcbae211f0c277a87c5280f7fb
Luis Portillo <l.portillo@geekshubs.com>
2021-12-21 15:52:49 +0100

    Added header in authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        $request->headers->set('accept', 'application/json', true);
+        
         if (!$request->expectsJson()) {
             return route('login');
         }
